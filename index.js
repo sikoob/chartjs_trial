7baff14f833650b17bfd96ff1110770ef6e6ffb7
sikoob <simon.koob@gmx.net>
2021-12-30 11:42:47 +0100

    adding loops dependent on number of years display to make setting up of constant value arrays easier

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 
 //definition of variables
 
-let dataSet = [660000000, 650000000, 610000000, 640000000];     //dataset to be used
+let dataSet = [670000000, 660000000, 650000000, 655000000, 650000000];     //dataset to be used
 
 let maxVal = Math.max(...dataSet);                              //max. value in dataset to define height of y-axes and way of chart setup
 
-let labelYears = ['2017','2018', '2019', '2020'];               //variable for years to be displayed for easier modification
+let labelYears = ['2016','2017','2018', '2019', '2020'];        //variable for years to be displayed for easier modification
 
-let stepSize = "";                                              //variable for step size, empty because defined later in if-statement dependent on chart setup
+let stepSizeLower = "";                                         //variable for step size for lower axis, empty because defined later in if-statement dependent on chart setup
+
+let stepSizeUpper = "";                                         //variable for step size for upper axis in broken setup, empty because defined later in if-statement dependent on chart setup
 
 let maxValCeil = "";                                            //variable to format max. Val to fit stepsize, empty because defined later in if-statement dependent on chart setup
 
@@ -17,6 +19,10 @@ let lowerAxesMax = "";                                          //variable max. 
 let upperAxesMin = "";                                          //variable for starting point for upper y-axis if used, empty because defined later in if-statement dependent on chart setup
 
 let data = "";                                                  // variable for data setup, defined in if-statement dependent on chart setup
+
+let dataFiller = [];                                            //variable for filler component in broken scale setup, empty array to be filled later
+
+let lowerData = [];                                             //variable for data for lower axis component in broken scale setup, empty array to be filled later
 
 let config = "";                                                //variable for chart configuration, defined in if-statement dependent on chart setup
 
@@ -27,13 +33,21 @@ if (maxVal > 100000000) {                                       //chooses chart 
 
     //setup for broken scale when maxVal > 100 Mio.
 
-    stepSize = 50000000;                                        //define step size 
+    stepSizeLower = 50000000;                                        //define step size for lower axis
 
-    maxValCeil = Math.ceil(maxVal/stepSize)*stepSize;           //format max. Val to fit stepsize
+    stepSizeUpper = 25000000;                                       //define step size for upper axis
 
-    lowerAxesMax = Math.ceil((maxVal/10)/stepSize)*stepSize;    //define max. value for the bottom y-axis
+    maxValCeil = Math.ceil(maxVal/stepSizeUpper)*stepSizeUpper;           //format max. Val to fit stepsize for upper axis
 
-    upperAxesMin = Math.ceil((maxVal-maxVal/10)/stepSize)*stepSize; //define starting point for upper y-axis
+    lowerAxesMax = Math.ceil((maxVal/10)/stepSizeLower)*stepSizeLower;    //define max. value for the lower y-axis
+
+    upperAxesMin = Math.floor((maxVal-maxVal/15)/stepSizeUpper)*stepSizeUpper; //define starting point for upper y-axis
+
+    //looping through labelYears to add data to filler and lower axis data setup, consistend data for both variables
+    for (let i = 0; i < labelYears.length; i++) {
+        dataFiller[i] = 1;
+        lowerData[i] = lowerAxesMax;
+    };
 
     //data setup for chart
 
@@ -57,7 +71,7 @@ if (maxVal > 100000000) {                                       //chooses chart 
             //filler for the area between the lower and upper axes
             label: 'filler',
             yAxisID: 'filler',
-            data: [1, 1, 1, 1],                             //easier setup for categorical axis to be functioning as a filler
+            data: dataFiller,                               //easier setup for categorical axis to be functioning as a filler by using looped variable
             pointRadius: 0,                                 //not displaying a datapoint for interaction
             backgroundColor: [
                 pattern.draw('diagonal','rgb(18, 50, 80, 0.2)') //using the Patternomaly JS package to show area that is used as a 'breaker'
@@ -71,7 +85,7 @@ if (maxVal > 100000000) {                                       //chooses chart 
             //data to fill area for lower axis
             label: 'lower',
             yAxisID: 'lower',
-            data: [lowerAxesMax, lowerAxesMax, lowerAxesMax, lowerAxesMax], //data dependent on lower Axes max. value; used to fill whole area
+            data: lowerData,                                //data dependent on lower Axes max. value; used to fill whole area by using looped variable
             pointRadius: 0,                                 //not displaying a datapoint for interaction
             backgroundColor: [
                 'rgba(18, 50, 80, 0.2)'
@@ -105,7 +119,7 @@ if (maxVal > 100000000) {                                       //chooses chart 
                         callback: function(value, index, values) {
                             return '€ ' + value / 1000000 + " Mio.";    //display formatting on numbers to be display on axis
                         },
-                        stepSize: stepSize,
+                        stepSize: stepSizeLower,
                         font: {
                             size: 11
                         }
@@ -132,14 +146,14 @@ if (maxVal > 100000000) {                                       //chooses chart 
                     position: 'left',
                     stacked: true,
                     stack: 'broken',
-                    stackWeight: 1.5,                   //used to display upper axis area the largest
+                    stackWeight: 1.4,                   //used to display upper axis area the largest
                     max: maxValCeil,                    //dependent on data input and step size
                     min: upperAxesMin,                  //dependent on data input and step size
                     ticks: {
                         callback: function(value, index, values) {
                             return '€ ' + value/ 1000000 + ' Mio.'; //display formatting on numbers to be display on axis
                         },
-                        stepSize: stepSize,
+                        stepSize: stepSizeUpper,
                         font: {
                             size: 11
                         }
@@ -156,9 +170,9 @@ if (maxVal > 100000000) {                                       //chooses chart 
     };
 } else {
     //define chart setup when maxVal is smaller than 100 Mio.
-    stepSize = 20000000;                               //smaller step size to better display data
+    stepSizeLower = 20000000;                                   //smaller step size to better display data, only using lower axis
 
-    maxValCeil = Math.ceil(maxVal/stepSize)*stepSize; //format max. Val to fit stepsize
+    maxValCeil = Math.ceil(maxVal/stepSizeLower)*stepSizeLower; //format max. Val to fit stepsize
 
     //data setup for chart
 
@@ -198,7 +212,7 @@ if (maxVal > 100000000) {                                       //chooses chart 
                         callback: function(value, index, values) {
                             return '€ ' + value/ 1000000 + ' Mio.'; //display formatting on numbers to be display on axis
                         },
-                        stepSize: stepSize,
+                        stepSize: stepSizeLower,
                         font: {
                             size: 11
                         }
